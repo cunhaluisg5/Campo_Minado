b5bb9ad2133cc16bc550207c8b83995864fc6b53
Luis Cipriani <cunhaluisg5@gmail.com>
2019-12-19 11:29:30 -0300

    Criado os parâmetros do jogo

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import params from './src/params'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Iniciando o Mines!!!</Text>
+        <Text style={styles.instructions}>Tamanho da grade:
+          {params.getRowsAmount()}x{params.getColumnsAmount()}</Text>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
@@ -16,4 +21,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center'
+  }
 });
